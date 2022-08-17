4539b0b959dd86ec1ea4a8d5d8cfcd1f5e3e9ef9
mastervalor <101042117+mastervalor@users.noreply.github.com>
2022-08-17 16:42:16 -0700

    Add files via upload

---
+++ belt_exam.sql
@@ -0,0 +1,118 @@
+CREATE DATABASE  IF NOT EXISTS `belt_exam` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `belt_exam`;
+-- MySQL dump 10.13  Distrib 8.0.29, for macos12 (x86_64)
+--
+-- Host: localhost    Database: belt_exam
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `likes`
+--
+
+DROP TABLE IF EXISTS `likes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `likes` (
+  `show_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`show_id`,`user_id`),
+  KEY `fk_shows_has_users_users1_idx` (`user_id`),
+  KEY `fk_shows_has_users_shows_idx` (`show_id`),
+  CONSTRAINT `fk_shows_has_users_shows` FOREIGN KEY (`show_id`) REFERENCES `shows` (`id`),
+  CONSTRAINT `fk_shows_has_users_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `likes`
+--
+
+LOCK TABLES `likes` WRITE;
+/*!40000 ALTER TABLE `likes` DISABLE KEYS */;
+INSERT INTO `likes` VALUES (3,1),(4,1),(1,2),(1,3),(3,3),(4,3);
+/*!40000 ALTER TABLE `likes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shows`
+--
+
+DROP TABLE IF EXISTS `shows`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shows` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(225) DEFAULT NULL,
+  `network` varchar(225) DEFAULT NULL,
+  `release_date` date DEFAULT NULL,
+  `description` text,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `poster_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_shows_users1_idx` (`poster_id`),
+  CONSTRAINT `fk_shows_users1` FOREIGN KEY (`poster_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shows`
+--
+
+LOCK TABLES `shows` WRITE;
+/*!40000 ALTER TABLE `shows` DISABLE KEYS */;
+INSERT INTO `shows` VALUES (1,'the simpsons','fox_network','2022-08-23','family show','2022-08-17 10:40:36','2022-08-17 10:40:36',1),(3,'familey guy','fox network','2022-08-03','ecwdcc','2022-08-17 12:45:55','2022-08-17 12:45:55',2),(4,'you know ','you know','2022-08-18','you know','2022-08-17 12:46:16','2022-08-17 12:46:16',2),(6,'testing 3','testing 3','2022-08-11','ecwvwdvwdv','2022-08-17 13:34:46','2022-08-17 13:34:46',3);
+/*!40000 ALTER TABLE `shows` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(225) DEFAULT NULL,
+  `last_name` varchar(225) DEFAULT NULL,
+  `email` varchar(225) DEFAULT NULL,
+  `password` varchar(225) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'adam','jenson','adamj@gmail.com','$2b$12$l4blSkW3UaEZQsB0u/gbP.nm0mpFi5WQebKBj7jK.n1/zPpShwV7S','2022-08-17 10:30:37','2022-08-17 10:30:37'),(2,'mark','hamel','markh@gmail.com','$2b$12$4LjhW3vJwB53KLnDAzqVhOCQ9mDksVqbplKySoVAcuXOamXm7WvB.','2022-08-17 10:47:23','2022-08-17 10:47:23'),(3,'robert','son','rs@gamil.com','$2b$12$m7dpkzEoYgL94ZsS45WONO6nBy/qW1/zX0/jGk2OMmF0IbtApEd4i','2022-08-17 13:00:44','2022-08-17 13:00:44');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-17 13:56:42
